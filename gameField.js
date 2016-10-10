da5c2a8b7033081f266d236b99a5bc1c210a14dc
kopa4al2 <a582hs@gmail.com>
2016-10-10 21:11:19 +0300

    Jquery added
    Jquery added

--- gameField.js
+++ gameField.js
@@ -1,22 +1,31 @@
+$(document).ready(init);
 function init() {
-    var matrix = [
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1]
-    ];
-
+    var background = new Image();
+    background.src = 'images/background.png';
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
+    ctx.drawImage(background,0, 0 , 480, 320, 0 ,0 ,800, 600);
+    var matrix = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
     var land = new Image();
     land.src="images/sprite.png";
     let row = "";
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 6; j++) {
+    for (let i = 0; i < 16; i++) {//redove na matrix
+        for (let j = 0; j < 12; j++) {//koloni na matrix
             if ( matrix[j][i] === 1 ){
-                ctx.drawImage(land,0,0,58,58,i*100,j*100,101,101)
+                ctx.drawImage(land,0,0,58,58,i*50,j*50,50,50)
             }
         }
     }
