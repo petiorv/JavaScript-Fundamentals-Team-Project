6f712cd222a478b4ed572eeaf179abcf5a61977e
kopa4al2 <a582hs@gmail.com>
2016-10-10 21:32:49 +0300

    Latest

--- gameField.js
+++ gameField.js
@@ -2,9 +2,10 @@ $(document).ready(init);
 function init() {
     var background = new Image();
     background.src = 'images/background.png';
+    var mainCharImg = new Image();
+    mainCharImg.src = 'images/mainCharacter.gif';
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    ctx.drawImage(background,0, 0 , 480, 320, 0 ,0 ,800, 600);
     var matrix = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -12,7 +13,7 @@ function init() {
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 'H', 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -20,14 +21,61 @@ function init() {
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
     var land = new Image();
-    land.src="images/sprite.png";
+    land.src = "images/sprite.png";
     let row = "";
-    for (let i = 0; i < 16; i++) {//redove na matrix
-        for (let j = 0; j < 12; j++) {//koloni na matrix
-            if ( matrix[j][i] === 1 ){
-                ctx.drawImage(land,0,0,58,58,i*50,j*50,50,50)
+    var charPos = {
+        x: 6,
+        y: 8
+    };
+
+    function update() {
+        ctx.clearRect(0, 0 , 800, 600);
+        ctx.drawImage(background, 0, 0, 480, 320, 0, 0, 800, 600);
+        for (let i = 0; i < 16; i++) {//redove na matrix
+            for (let j = 0; j < 12; j++) {//koloni na matrix
+                if (matrix[j][i] === 1) {
+                    ctx.drawImage(land, 0, 0, 58, 58, i * 50, j * 50, 50, 50)
+                }
+                if (matrix[j][i] === 'H') {
+                    ctx.drawImage(mainCharImg, 0, 0, 64, 64, i * 50, j * 50, 50, 50)
+                    charPos.x = j;
+                    charPos.y = i;
+                }
             }
         }
     }
+
+    (window).addEventListener('keydown', function (event) {
+        switch (event.code) {
+            case 'ArrowLeft' :
+                if(matrix[charPos.x][charPos.y-1] != 1){
+                matrix[charPos.x][charPos.y] = 0;
+                matrix[charPos.x][charPos.y-1] = 'H';
+                update();
+                }
+                break;
+            case 'ArrowRight' :
+                if(matrix[charPos.x][charPos.y+1] != 1){
+                    matrix[charPos.x][charPos.y] = 0;
+                    matrix[charPos.x][charPos.y+1] = 'H';
+                    update();
+                }
+                break;
+            case 'ArrowDown' :
+                if(matrix[charPos.x+1][charPos.y] != 1){
+                    matrix[charPos.x][charPos.y] = 0;
+                    matrix[charPos.x+1][charPos.y] = 'H';
+                    update();
+                }
+                break;
+            case 'ArrowUp' :
+                if(matrix[charPos.x-1][charPos.y] != 1){
+                    matrix[charPos.x][charPos.y] = 0;
+                    matrix[charPos.x-1][charPos.y] = 'H';
+                    update();
+                }
+                break;
+        }
+    })
 
 }
