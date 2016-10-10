8a2f8909c4068aaa3a210b1b695c4f94ce2f0c51
kopa4al2 <a582hs@gmail.com>
2016-10-11 00:21:25 +0300

    Character rotation

--- gameField.js
+++ gameField.js
@@ -1,84 +1,125 @@
 $(document).ready(init);
 function init() {
+
+    const BLOCK_HEIGHT = 50;
+    const BLOCK_WIDTH = 50;
+
     var background = new Image();
     background.src = 'images/background.png';
     var mainCharImg = new Image();
     mainCharImg.src = 'images/mainCharacter.gif';
     var needsToBe = new Image();
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var matrix = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 'H', 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ];
+    needsToBe.src = 'images/toBePut.bmp';
     var land = new Image();
     land.src = "images/sprite.png";
-    let row = "";
+
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var gameField = [    //----------------------------> X RASTE HORIZONTALNO
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'X', 1],  //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |  Y RASTE NADOLO
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],    //  |
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]     //  |
+    ];
+
     var charPos = {
-        x: 6,
+        x: 5,
         y: 8
     };
 
-    function update() {
-        ctx.clearRect(0, 0 , 800, 600);
+    var direction = '';
+
+    function update(direction) {
+        ctx.clearRect(0, 0, 800, 600);
         ctx.drawImage(background, 0, 0, 480, 320, 0, 0, 800, 600);
-        for (let i = 0; i < 16; i++) {//redove na matrix
-            for (let j = 0; j < 12; j++) {//koloni na matrix
-                if (matrix[j][i] === 1) {
-                    ctx.drawImage(land, 0, 0, 58, 58, i * 50, j * 50, 50, 50)
+        for (let row = 0; row < gameField.length; row++) {//redove na matrix
+            for (let col = 0; col < gameField[row].length; col++) {//koloni na matrix
+                //PROVERKA KYDE SA STENI
+                if (gameField[row][col] === 1) {
+                    ctx.drawImage(land, 0, 0, 58, 58, col * BLOCK_HEIGHT, row * BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_WIDTH)
                 }
-                if (matrix[j][i] === 'H') {
+                //PROVERKA KYDE E GEROQ
+                if (gameField[row][col] === 'H') {
                     ctx.save();
-                    //ctx.translate(i*50+50, j*50);
-                    //ctx.rotate(Math.PI/2);
-                    ctx.drawImage(mainCharImg, 0, 0, 64, 64, 0, 0, 50, 50)
-                    charPos.x = j;
-                    charPos.y = i;
+                    //PROVERKA NAKKYDE SE DVIJI
+                    switch (direction) {
+                        case 'R' :
+                            //             IZMESTVANE NALQVO NADQSNO    ,//IZMESTVANO NAGORE NADOLO
+                            ctx.translate(charPos.y * BLOCK_HEIGHT + 100, charPos.x * BLOCK_WIDTH);
+                            ctx.rotate(Math.PI / 2);
+                            break;
+                        case 'U' :
+                            //           IZMESTVANE NALQVO NADQSNO, //IZMESTVANO NAGORE NADOLO
+                            ctx.translate(charPos.y * BLOCK_HEIGHT, charPos.x * BLOCK_WIDTH - 50);
+                            ctx.rotate(Math.PI / 90);
+                            break;
+                        case 'L' :
+                            //             IZMESTVANE NALQVO NADQSNO   , //IZMESTVANO NAGORE NADOLO
+                            ctx.translate(charPos.y * BLOCK_HEIGHT - 50, charPos.x * BLOCK_WIDTH + 50);
+                            ctx.rotate(15 * Math.PI / 2);
+                            break;
+                        case 'D' :
+                            //             IZMESTVANE NALQVO NADQSNO   , //IZMESTVANO NAGORE NADOLO
+                            ctx.translate(charPos.y * BLOCK_HEIGHT + 50, charPos.x * BLOCK_WIDTH + 100);
+                            ctx.rotate(Math.PI);
+                            break;
+                    }
+                    ctx.drawImage(mainCharImg, 0, 0, 64, 64, 0, 0, BLOCK_HEIGHT, BLOCK_WIDTH)
+                    charPos.x = row;
+                    charPos.y = col;
                     ctx.restore();
-                    ctx.strokeRect(charPos.y*50,charPos.x*50,50, 50);
                 }
+                //PROVERKA KYDE TRQBVA DA SE BUTNAT KUTIIKITE
+                // if (gameField[row][col] === 'X') {
+                //     ctx.drawImage(needsToBe, 0, 0, 64, 63, col*BLOCK_HEIGHT, row*BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_WIDTH)
+                //  }
             }
         }
     }
 
+    //proverka koe kopche e natisnato
     (window).addEventListener('keydown', function (event) {
         switch (event.code) {
             case 'ArrowLeft' :
-                if(matrix[charPos.x][charPos.y-1] != 1){
-                matrix[charPos.x][charPos.y] = 0;
-                matrix[charPos.x][charPos.y-1] = 'H';
-                update();
+                if (gameField[charPos.x][charPos.y - 1] != 1) {
+                    gameField[charPos.x][charPos.y] = 0;
+                    gameField[charPos.x][charPos.y - 1] = 'H';
+                    direction = 'L';
+                    update(direction);
                 }
+
                 break;
             case 'ArrowRight' :
-                if(matrix[charPos.x][charPos.y+1] != 1){
-                    matrix[charPos.x][charPos.y] = 0;
-                    matrix[charPos.x][charPos.y+1] = 'H';
-                    update();
+                if (gameField[charPos.x][charPos.y + 1] != 1) {
+                    gameField[charPos.x][charPos.y] = 0;
+                    gameField[charPos.x][charPos.y + 1] = 'H';
+                    direction = 'R';
+                    update(direction);
                 }
                 break;
             case 'ArrowDown' :
-                if(matrix[charPos.x+1][charPos.y] != 1){
-                    matrix[charPos.x][charPos.y] = 0;
-                    matrix[charPos.x+1][charPos.y] = 'H';
-                    update();
+                if (gameField[charPos.x + 1][charPos.y] != 1) {
+                    gameField[charPos.x][charPos.y] = 0;
+                    gameField[charPos.x + 1][charPos.y] = 'H';
+                    direction = 'D';
+                    update(direction);
                 }
                 break;
             case 'ArrowUp' :
-                if(matrix[charPos.x-1][charPos.y] != 1){
-                    matrix[charPos.x][charPos.y] = 0;
-                    matrix[charPos.x-1][charPos.y] = 'H';
-                    update();
+                if (gameField[charPos.x - 1][charPos.y] != 1) {
+                    gameField[charPos.x][charPos.y] = 0;
+                    gameField[charPos.x - 1][charPos.y] = 'H';
+                    direction = 'U';
+                    update(direction);
                 }
                 break;
         }
